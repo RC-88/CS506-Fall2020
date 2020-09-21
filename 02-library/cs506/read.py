5cb02d4e035e5d324ed17a95953737b2757f5182
RC-88 <lilychau999@gmail.com>
2020-09-21 04:41:17 -0400

    Implemented python library exercise

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,24 @@
 def read_csv(csv_file_path):
-    """
-        Given a path to a csv file, return a matrix (list of lists)
-        in row major.
-    """
-    raise NotImplementedError()
+    data=[]
+    f=open(csv_file_path)
+    ln=f.readline()
+    while ln:
+        string=[str(x).replace('"', "") for x in ln.strip().split(",")]
+        print(string)
+        for i in range(len(string)):
+            if string[i].isdigit():
+                string[i] = int(string[i])
+            else:
+               string[i] = str(string[i])
+
+        data.append(string)
+        ln = f.readline()
+    return data
+
+
+#print(read_csv("/Users/reinachau/CS506-Fall2020/02-library/tests/test_files/dataset_2.csv"))
+
+
+
+
+
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,74 @@
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if x==[] and y==[]:
+        return "lengths must not be zero"
+    elif len(x) > len(y) or len(x) < len(y):
+        return "lengths must be equal"
+    else:
+        distancesum = 0
+        
+        for i in range(len(x)):
+            distancesum += (x[i]-y[i])**2
+            
+        d = distancesum**(1/2)
+        return d 
+
+#test euclidean_dist([0,0], [1,0]) == 1
+#print(euclidean_dist([0,0], [1,0])) 
+#test euclidean_dist([0,0,0], [1,0,0]) == 1
+#print(euclidean_dist([0,0,0], [1,0,0]))
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if x==[] and y==[]:
+        return "lengths must not be zero"
+    elif len(x) > len(y) or len(x) < len(y):
+        return "lengths must be equal"
+    else:
+        d = 0
+          
+        for i in range(len(x)): 
+            d += abs(x[i] - y[i])
+                
+        return d
+
+#test manhattan_dist([0,0], [1,1]) = 2
+#print(manhattan_dist([0,0], [1,1]))
+#test sim.manhattan_dist([0,0,0], [1,1,1]) == 3
+#print(manhattan_dist([0,0,0], [1,1,1]))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if x==[] and y==[]:
+        return "lengths must not be zero"
+    else:
+        intersection = len(list(set(x).intersection(y)))
+        union = len(list(set(x).union(y)))
+        d = 1 - float(intersection / union)
+        return(d)
+
+#test jaccard_dist([0,0], [1,0]) == .5
+#print(jaccard_dist([0,0], [1,0]))
+#test accard_dist([0,0,0], [1,1,1]) == 1
+#print(jaccard_dist([0,0,0], [1,1,1]))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if x==[] and y==[]:
+        return "lengths must not be zero"
+    elif len(x) > len(y) or len(x) < len(y):
+        return "lengths must be equal"
+    else:
+        sumxx=0; sumyy=0; sumxy=0;
+        
+        for i in range(len(x)):
+            sumxx += x[i]*x[i]
+            sumyy += y[i]*y[i]
+            sumxy += x[i]*y[i]
+        
+        d = sumxy/(sumxx*sumyy)**(1/2)
+        return(d)
+
+#test cosine_sim([1,0], [1,0]) == 1
+#print(cosine_sim([1,0], [1,0]))
+#test cosine_sim([0,1,0], [1,0,0]) == 0
+#print(cosine_sim([0,1,0], [1,0,0]))
 
 # Feel free to add more
